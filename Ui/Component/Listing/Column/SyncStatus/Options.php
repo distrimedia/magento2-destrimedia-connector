26bae9a2883f9437b8ac2df50a4b5e6a22e18de1
Tristan Hofman <tristan@baldwin.be>
2020-12-01 15:51:05 +0100

    another attempt on fixing the options

--- Ui/Component/Listing/Column/SyncStatus/Options.php
+++ Ui/Component/Listing/Column/SyncStatus/Options.php
@@ -10,10 +10,10 @@ use Magento\Framework\Data\OptionSourceInterface;
 class Options implements OptionSourceInterface
 {
     const SYNC_STATUS_NOT_SYNCED = 0;
-    const SYNC_STATUS_SYNCED     = 1;
-    const SYNC_STATUS_RETRY      = 2;
-    const SYNC_STATUS_FAILED     = 3;
-    const SYNC_STATUS_PENDING_CANCELED   = 4;
+    const SYNC_STATUS_SYNCED = 1;
+    const SYNC_STATUS_RETRY = 2;
+    const SYNC_STATUS_FAILED = 3;
+    const SYNC_STATUS_PENDING_CANCELED = 4;
 
     const SYNC_STATUS_RCV = 5;
     const SYNC_STATUS_PCK = 6;
@@ -29,7 +29,7 @@ class Options implements OptionSourceInterface
     const STATUS_IS_BEING_PROCESSED = 'SCN';
     const STATUS_PACKED_AND_WAITED_FOR_SHIPMENT_LABEL = 'RDY';
     const STATUS_PACKED_AND_LABELED_WAITING_FOR_SHIPMENT = 'LBL';
-    const STATUS_SHIPPED  = 'SHP';
+    const STATUS_SHIPPED = 'SHP';
     const STATUS_PARTLY_SHIPPED = 'PSH';
     const STATUS_CANCELLED = 'CNL';
 
@@ -44,15 +44,16 @@ class Options implements OptionSourceInterface
             ['value' => self::SYNC_STATUS_NOT_SYNCED, 'label' => __('Not Synced')],
             ['value' => self::SYNC_STATUS_SYNCED, 'label' => __('Synced')],
             ['value' => self::SYNC_STATUS_RETRY, 'label' => __('Retrying')],
-            ['value' => self::SYNC_STATUS_FAILED,   'label' => __('Failed')],
-            ['value' => self::SYNC_STATUS_PENDING_CANCELED,   'label' => __('Pending Cancel')],
-            ['value' => self::STATUS_RECEIVED_IN_SYSTEM,   'label' => __('Received in system')],
-            ['value' => self::STATUS_READY_FOR_PICKING,   'label' => __('Ready for picking')],
-            ['value' => self::STATUS_PACKED_AND_WAITED_FOR_SHIPMENT_LABEL,   'label' => __('Packed, waiting for shipment label')],
-            ['value' => self::STATUS_PACKED_AND_LABELED_WAITING_FOR_SHIPMENT,   'label' => __('Packed and labeled, waiting for shipment')],
-            ['value' => self::STATUS_SHIPPED,   'label' => __('Shipped')],
-            ['value' => self::STATUS_PARTLY_SHIPPED,   'label' => __('Partly Shipped')],
-            ['value' => self::STATUS_CANCELLED,   'label' => __('Canceled')],
+            ['value' => self::SYNC_STATUS_FAILED, 'label' => __('Failed')],
+            ['value' => self::SYNC_STATUS_PENDING_CANCELED, 'label' => __('Pending Cancel')],
+            ['value' => self::SYNC_STATUS_RCV, 'label' => __('Received in system')],
+            ['value' => self::SYNC_STATUS_PCK, 'label' => __('Ready for picking')],
+            ['value' => self::SYNC_STATUS_SCN, 'label' => __('Order is being processed / picked')],
+            ['value' => self::SYNC_STATUS_RDY, 'label' => __('Packed, waiting for shipment label')],
+            ['value' => self::SYNC_STATUS_LBL, 'label' => __('Packed and labeled, waiting for shipment')],
+            ['value' => self::SYNC_STATUS_SHP, 'label' => __('Shipped')],
+            ['value' => self::SYNC_STATUS_PSH, 'label' => __('Partly Shipped')],
+            ['value' => self::SYNC_STATUS_CNL, 'label' => __('Canceled')],
         ];
     }
 
@@ -63,9 +64,10 @@ class Options implements OptionSourceInterface
     public static function getDistriMediaStatusses()
     {
         return [
-           self::STATUS_RECEIVED_IN_SYSTEM => self::SYNC_STATUS_RCV,
+            self::STATUS_RECEIVED_IN_SYSTEM => self::SYNC_STATUS_RCV,
             self::STATUS_READY_FOR_PICKING => self::SYNC_STATUS_PCK,
-            self::STATUS_PACKED_AND_WAITED_FOR_SHIPMENT_LABEL  => self::SYNC_STATUS_RDY,
+            self::STATUS_IS_BEING_PROCESSED => self::SYNC_STATUS_SCN,
+            self::STATUS_PACKED_AND_WAITED_FOR_SHIPMENT_LABEL => self::SYNC_STATUS_RDY,
             self::STATUS_PACKED_AND_LABELED_WAITING_FOR_SHIPMENT => self::SYNC_STATUS_LBL,
             self::STATUS_SHIPPED => self::SYNC_STATUS_SHP,
             self::STATUS_PARTLY_SHIPPED => self::SYNC_STATUS_PSH,
